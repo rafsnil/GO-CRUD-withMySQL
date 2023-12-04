0c299d7591add59d891cd0c6ca1eb078968cd7e9
rafsnil <rafsnil@gmail.com>
2023-12-04 14:40:02 +0600

    Worked on Models and Main, I need a break now :')

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+
+	// "github.com/jinzhu/gorm/dialects/mysql"
+	routes "github.com/rafsnil/CRUD-WITH-MySQL/pkg/Routes"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:9010", r))
+}
--- pkg/Routes/book-routes.go
+++ pkg/Routes/book-routes.go
@@ -2,13 +2,13 @@ package routes
 
 import (
 	"github.com/gorilla/mux"
-	"github.com/rafsnil/CRUD-WITH-MySQL/pkg/Controllers"
+	controllers "github.com/rafsnil/CRUD-WITH-MySQL/pkg/Controllers"
 )
 
 var RegisterBookStoreRoutes = func(router *mux.Router) {
-	router.HandleFunc("/book", Controllers.CreateBook).Methods("POST")
-	router.HandleFunc("/book", Controllers.GetBook).Methods("GET")
-	router.HandleFunc("/book/{bookId}", Controllers.GetBookById).Methods("GET")
-	router.HandleFunc("/book/{bookId}", Controllers.UpdateBook).Methods("PUT")
-	router.HandleFunc("/book/{bookId}", Controllers.DeleteBook).Methods("DELETE")
+	router.HandleFunc("/book", controllers.CreateBook).Methods("POST")
+	router.HandleFunc("/book", controllers.GetBook).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.GetBookById).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.UpdateBook).Methods("PUT")
+	router.HandleFunc("/book/{bookId}", controllers.DeleteBook).Methods("DELETE")
 }
